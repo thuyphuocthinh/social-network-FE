31b9c94577f185ae1beecb011bdf2d5206b1a46c
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-09-19 22:48:06 +0700

    Added mobile header, added profile header

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -10,10 +10,74 @@ import {
   faList,
 } from "@fortawesome/free-solid-svg-icons";
 import "./Header.scss";
+import { useEffect, useRef, useState } from "react";
 
 library.add(faSearch, faHome, faUserFriends, faUser, faMessage, faList);
 
 export default function Header() {
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccount, setShowAccount] = useState(false);
+  const searchMobileRef = useRef(null);
+  const searchMobileIconRef = useRef(null);
+  const menuMobileRef = useRef(null);
+  const menuMobileIconRef = useRef(null);
+  const accountIconRef = useRef(null);
+  const menuAccountRef = useRef(null);
+
+  const handleCloseSearchMobile = (e) => {
+    const element = e.target;
+    if (!showMobileSearch) {
+      if (
+        element === searchMobileIconRef.current ||
+        element.closest("span") === searchMobileIconRef.current
+      ) {
+        setShowMobileSearch(true);
+      }
+    } else {
+      if (e.target !== searchMobileRef.current) setShowMobileSearch(false);
+    }
+  };
+
+  const handleCloseMenuMobile = (e) => {
+    const element = e.target;
+    if (!showMobileMenu) {
+      if (
+        element === menuMobileIconRef.current ||
+        element.closest("li") === menuMobileIconRef.current
+      ) {
+        setShowMobileMenu(true);
+      }
+    } else {
+      if (e.target !== menuMobileRef.current) setShowMobileMenu(false);
+    }
+  };
+
+  const handleCLoseAccountMenu = (e) => {
+    const element = e.target;
+    if (!showAccount) {
+      if (
+        element === accountIconRef.current ||
+        element.closest("li") === accountIconRef.current
+      ) {
+        setShowAccount(true);
+      }
+    } else {
+      if (e.target !== menuAccountRef.current) setShowAccount(false);
+    }
+  }
+  
+  useEffect(() => {
+    window.addEventListener("click", handleCloseSearchMobile);
+    window.addEventListener("click", handleCloseMenuMobile);
+    window.addEventListener("click", handleCLoseAccountMenu);
+    return () => {
+      removeEventListener("click", handleCloseSearchMobile);
+      removeEventListener("click", handleCloseMenuMobile);
+      removeEventListener("click", handleCLoseAccountMenu);
+    };
+  });
+
   return (
     <div className="header">
       <div className="container-fluid">
@@ -30,6 +94,20 @@ export default function Header() {
                 id="keyword"
                 placeholder="Search"
               />
+            </div>
+            <div className="header-search--mobile">
+              <span ref={searchMobileIconRef}>
+                <FontAwesomeIcon icon="search" className="search-icon" />
+              </span>
+              {showMobileSearch && (
+                <input
+                  type="search"
+                  name="keyword"
+                  id="keyword"
+                  placeholder="Search"
+                  ref={searchMobileRef}
+                />
+              )}
             </div>
           </div>
           <div className="header-middle d-sm-block d-none">
@@ -48,20 +126,54 @@ export default function Header() {
           </div>
           <div className="header-right">
             <ul className="header-list">
-              <li className="header-item d-flex d-sm-none">
+              <li className="header-item d-flex d-sm-none" ref={menuMobileIconRef}>
                 <NavLink className="header-link">
                   <FontAwesomeIcon icon="list" />
                 </NavLink>
+                {showMobileMenu && (
+                  <div className="header-menu--mobile" ref={menuMobileRef}>
+                    <ul className="header-menu--mobile-list">
+                      <li className="header-menu--mobile-item">
+                        <NavLink className="header-menu--mobile-link">
+                          <FontAwesomeIcon icon="home" />
+                          <p>Home</p>
+                        </NavLink>
+                      </li>
+                      <li className="header-menu--mobile-item">
+                        <NavLink className="header-menu--mobile-link">
+                          <FontAwesomeIcon icon="user-friends" />
+                          <p>Users</p>
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </div>
+                )}
               </li>
               <li className="header-item">
                 <NavLink className="header-link">
                   <FontAwesomeIcon icon="message" />
                 </NavLink>
               </li>
-              <li className="header-item">
+              <li className="header-item" ref={accountIconRef}>
                 <NavLink className="header-link">
                   <FontAwesomeIcon icon="user" />
                 </NavLink>
+                {
+                  showAccount && <div className="header-account" ref={menuAccountRef}>
+                    <ul className="header-account-list">
+                      <li className="header-account-item">
+                        <NavLink className="header-account-link">
+                          Profile
+                        </NavLink>
+                      </li>
+                      <li className="header-account-item">
+                        <NavLink className="header-account-link">
+                          Logout
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </div>
+                }
               </li>
             </ul>
           </div>
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,5 +1,33 @@
+import { Fragment } from "react";
 import Header from "../../components/Home/Header";
+import "./Profile.scss";
 
 export default function Profile() {
-  return <Header />;
+  return (
+    <Fragment>
+      <Header />
+      <div className="profile">
+        <div className="profile-head">
+          <div className="profile-head-container">
+            <div className="profile-head-cover"></div>
+            <div className="profile-head-user">
+              <div className="profile-avatar">
+                <div></div>
+              </div>
+              <div className="profile-basic-info">
+                <h3>Thủy Phước Thịnh</h3>
+                <p>388 friends</p>
+              </div>
+              <div className="profile-actions">
+                <button className="btn btn-friends">Friends</button>
+                <button className="btn btn-message">Message</button>
+              </div>
+            </div>
+            <div className="profile-head-tabs"></div>
+          </div>
+        </div>
+        <div className="profile-body"></div>
+      </div>
+    </Fragment>
+  );
 }
